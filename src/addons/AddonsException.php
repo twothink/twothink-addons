48a45849b54f662f8279754390d003d923c7fa01
艺品网络 <593657688@qq.com>
2017-11-19 16:36:11 +0800

    代码升级

---
+++ src/addons/AddonsException.php
@@ -0,0 +1,27 @@
+<?php
+// +----------------------------------------------------------------------
+// | TwoThink [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 http://www.twothink.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 艺品网络  82550565@qq.com <www.twothink.cn> 
+// +----------------------------------------------------------------------
+namespace think\addons;
+
+use think\Exception;
+
+/**
+ * 插件异常处理类
+ * @package think\addons
+ */
+class AddonsException extends Exception
+{
+
+    public function __construct($message, $code, $data = '')
+    {
+        $this->message  = $message;
+        $this->code     = $code;
+        $this->data     = $data;
+    }
+
+}
